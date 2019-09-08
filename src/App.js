d1e73022e8a622743dd7839bcb874bf0252cfd04
Stefan Wille <github@stefanwille.com>
2019-09-08 12:08:33 +0200

    .

--- src/App.js
+++ src/App.js
@@ -1,26 +1,91 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, createContext, useContext } from 'react';
 import './App.css';
 
+import { configure, observable, action } from 'mobx';
+import { observer } from 'mobx-react';
+
+configure({ enforceActions: 'observed' });
+
+const StoreContext = createContext();
+
+const AddTodo = () => {
+	const store = useContext(StoreContext);
+	const [ todo, setTodo ] = useState('Neu');
+	const handleChange = (event) => {
+		setTodo(event.target.value);
+	};
+	const handleKeyPress = (event) => {
+		if (event.key === 'Enter') {
+			store.addTodo(todo);
+		}
+	};
+	return (
+		<div>
+			<input type="text" value={todo} onChange={handleChange} onKeyPress={handleKeyPress} />
+		</div>
+	);
+};
+
+const DeleteButton = ({ index }) => {
+	const store = useContext(StoreContext);
+	const deleteTodo = store.deleteTodo;
+	const handleDeleteClick = () => {
+		deleteTodo(index);
+	};
+	return <button onClick={handleDeleteClick}>X</button>;
+};
+
+const NumberOfTodos = () => <h5>{store.todoCount} Todos</h5>;
+
+const TodoList = observer(() => {
+	const store = useContext(StoreContext);
+	return (
+		<div>
+			<ol>
+				{store.todos.map((todo, index) => (
+					<li key={index}>
+						{todo} <DeleteButton index={index} />
+					</li>
+				))}
+			</ol>
+			<NumberOfTodos />
+			<AddTodo />
+		</div>
+	);
+});
+
+const deleteFromArray = (array, index) => array.slice(0, index).concat(array.slice(index + 1));
+
+const store = observable(
+	{
+		todos: [ 'Buy milk', 'Write book' ],
+		get todoCount() {
+			return this.todos.length;
+		},
+		addTodo(todo) {
+			this.todos.push(todo);
+		},
+		deleteTodo(index) {
+			const newTodos = deleteFromArray(this.todos, index);
+			this.todos = newTodos;
+		}
+	},
+	{
+		addTodo: action.bound,
+		deleteTodo: action.bound
+	}
+);
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<StoreContext.Provider value={store}>
+			<div className="App">
+				<a href="https://reactjs.org">Learn React</a>
+
+				<TodoList />
+			</div>
+		</StoreContext.Provider>
+	);
 }
 
 export default App;
